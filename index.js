05e02f9749e191955657b0dfe43090b76b92d4de
Annia Forney <anniaforney@gmail.com>
2019-03-27 11:09:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
-function theBeatlesPlay(musicians,instruments) {
-  const array = [];
+function theBeatlesPlay(musicians, instruments) {
+  const arr = []
+
+  for (let i = 0, l = musicians.length; i < l; i++) {
+    arr.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+
+  return arr
 }
 
-for (let i = 0, 1 = musicians.length;i<1;i++) {
-  
+function johnLennonFacts(facts) {
+  const shoutedFacts = []
+
+  let i = 0
+  while (i < facts.length) {
+    shoutedFacts.push(`${facts[i]}!!!`)
+    i++
+  }
+
+  return shoutedFacts
+}
+
+function iLoveTheBeatles(n) {
+  const arr = []
+
+  do {
+    arr.push('I love the Beatles!')
+    n++
+  } while (n < 15)
+
+  return arr
 }
